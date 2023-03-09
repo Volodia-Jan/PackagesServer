2b4da244731566fca49f68259e13a9351f2abb3b
Bohdan Torbych <b.torbych@xtoniq.com>
2023-03-09 14:27:59 +0200

    Implemented infrastructure layer

---
+++ PackagesServer.Infrastructure/Repository/PackagesRepository.cs
@@ -0,0 +1,55 @@
+﻿using PackagesServer.Core.Entities;
+using PackagesServer.Core.Enumerations;
+using PackagesServer.Core.RepositoryContracts;
+
+namespace PackagesServer.Infrastructure.Repository;
+public class PackagesRepository : IPackagesRepository
+{
+    private int _id;
+    private readonly List<Package> _packages;
+
+    public PackagesRepository()
+    {
+        _id = 0;
+        _packages = new()
+        {
+            new Package { Id = ++_id, PackageIdentifier = "PKG001", Name = "Package 1", Description = "First package", Status = DeliverStatus.Received, RecipientId = 1, LastUpdate = DateTime.Now },
+            new Package { Id = ++_id, PackageIdentifier = "PKG002", Name = "Package 2", Description = "Second package", Status = DeliverStatus.Delivered, RecipientId = 2, LastUpdate = DateTime.Now },
+            new Package { Id = ++_id, PackageIdentifier = "PKG003", Name = "Package 3", Description = "Third package", Status = DeliverStatus.Received, RecipientId = 3, LastUpdate = DateTime.Now },
+            new Package { Id = ++_id, PackageIdentifier = "PKG004", Name = "Package 4", Description = "Fourth package", Status = DeliverStatus.Delivered, RecipientId = 4, LastUpdate = DateTime.Now },
+            new Package { Id = ++_id, PackageIdentifier = "PKG005", Name = "Package 5", Description = "Fifth package", Status = DeliverStatus.Received, RecipientId = 5, LastUpdate = DateTime.Now },
+            new Package { Id = ++_id, PackageIdentifier = "PKG006", Name = "Package 6", Description = "Sixth package", Status = DeliverStatus.Received, RecipientId = 6, LastUpdate = DateTime.Now },
+            new Package { Id = ++_id, PackageIdentifier = "PKG007", Name = "Package 7", Description = "Seventh package", Status = DeliverStatus.Delivered, RecipientId = 7, LastUpdate = DateTime.Now },
+            new Package { Id = ++_id, PackageIdentifier = "PKG008", Name = "Package 8", Description = "Eighth package", Status = DeliverStatus.Received, RecipientId = 8, LastUpdate = DateTime.Now },
+            new Package { Id = ++_id, PackageIdentifier = "PKG009", Name = "Package 9", Description = "Ninth package", Status = DeliverStatus.Delivered, RecipientId = 9, LastUpdate = DateTime.Now },
+            new Package { Id = ++_id, PackageIdentifier = "PKG010", Name = "Package 10", Description = "Tenth package", Status = DeliverStatus.Received, RecipientId = 10, LastUpdate = DateTime.Now }
+
+        };
+    }
+    public bool DeleteById(int id)
+    {
+        var countOfDeleted = _packages.RemoveAll(x => x.Id == id);
+
+        return countOfDeleted > 0;
+    }
+
+    public List<Package> FindAll() => _packages;
+
+    public Package? FindById(int id) =>
+        _packages.FirstOrDefault(x => x.Id == id);
+
+    public Package? Save(Package package)
+    {
+        if (_packages.Any(x => x.Id == package.Id))
+            return null;
+        package.Id = ++_id;
+        _packages.Add(package);
+
+        return package;
+    }
+
+    public Package? Update(Package package)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ PackagesServer.Infrastructure/Repository/RecipientsRepository.cs
@@ -0,0 +1,63 @@
+﻿using PackagesServer.Core.Entities;
+using PackagesServer.Core.RepositoryContracts;
+
+namespace PackagesServer.Infrastructure.Repository;
+public class RecipientsRepository : IRecipientsRepository
+{
+    private int _id;
+    private List<Recipient> _recipients;
+
+    public RecipientsRepository()
+    {
+        _id = 0;
+        _recipients = new List<Recipient>()
+        {
+            new Recipient { Id = ++_id, Name = "John Doe", Address = "123 Main St" },
+            new Recipient { Id = ++_id, Name = "Jane Smith", Address = "456 Oak St" },
+            new Recipient { Id = ++_id, Name = "Bob Johnson", Address = "789 Maple St" },
+            new Recipient { Id = ++_id, Name = "Samantha Lee", Address = "555 Pine St" },
+            new Recipient { Id = ++_id, Name = "David Chen", Address = "999 Elm St" },
+            new Recipient { Id = ++_id, Name = "Mary Kim", Address = "777 Cedar St" },
+            new Recipient { Id = ++_id, Name = "Tom Baker", Address = "444 Walnut St" },
+            new Recipient { Id = ++_id, Name = "Emily Davis", Address = "222 Spruce St" },
+            new Recipient { Id = ++_id, Name = "Michael Brown", Address = "333 Birch St" },
+            new Recipient { Id = ++_id, Name = "Lucy Wang", Address = "888 Ash St" }
+        };
+    }
+
+    public bool DeleteById(int id)
+    {
+        var toRemove = _recipients.SingleOrDefault(x => x.Id == id);
+        if (toRemove == null)
+            return true;
+
+        _recipients.Remove(toRemove);
+
+        return !_recipients.Any(x => x.Id == id);
+    }
+
+    public List<Recipient> FindAllRecipients() => _recipients;
+
+    public Recipient? FindById(int id) =>
+        _recipients.FirstOrDefault(x => x.Id == id);
+
+    public Recipient? Save(Recipient recipient)
+    {
+        if (_recipients.Any(x => x.Id == recipient.Id))
+            return null;
+        recipient.Id = ++_id;
+        _recipients.Add(recipient);
+
+        return recipient;
+    }
+
+    public Recipient? Update(Recipient recipient)
+    {
+        var toUpdate = FindById(recipient.Id);
+        if (toUpdate == null) return null;
+        toUpdate.Name = recipient.Name;
+        toUpdate.Address = recipient.Address;
+
+        return toUpdate;
+    }
+}
